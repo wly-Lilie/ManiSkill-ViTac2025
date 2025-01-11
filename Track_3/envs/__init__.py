61221703c5ab709c6b5fcb270651cf37d8db916c
wly-Lilie <wu_long_yan@163.com>
2025-01-11 20:11:27 +0800

    Delete Track_3/envs directory

--- Track_3/envs/__init__.py
+++
@@ -1,6 +0,0 @@
-from gymnasium.envs.registration import make, register, registry, spec
-
-register(
-    id="PegInsertionRandomizedMarkerEnv-v1",
-    entry_point="envs.peg_insertion:PegInsertionSimMarkerFLowEnv",
-)
